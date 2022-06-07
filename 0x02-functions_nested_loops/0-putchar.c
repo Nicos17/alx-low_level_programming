ba0aefdc2032fdf9cfd908a73cebc9431c03c3fe
Nicos17 <nicosgbenatohounvio17@gmail.com>
2022-06-07 08:48:51 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+# inclure  " main.h "
+# inclure  < chaîne.h >
+
+/* *
+* main - Imprime les nombres entre 00 et 89.
+*
+* Retour : Toujours 0 (Succès)
+*/
+int  principal ( vide )
+{
+	char word[ 9 ] = " _putchar " ;
+	entier je = 0 ;
+
+	pour (i = 0 ; i < 8 ; i++)
+		_putchar (mot[i]);
+
+	_putchar ( ' \n ' );
+
+	retour ( 0 );
+}
